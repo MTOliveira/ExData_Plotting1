531087127b0e20dc0bae9d6cef39a5c4a990b949
MTOliveira <38473374+MTOliveira@users.noreply.github.com>
2018-06-27 10:45:18 +0100

    Update Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -1,10 +1,19 @@
-## Plot 1
+##Plot 1
 
+#reading the dataset
 data <- read.table("R datasets/household_power_consumption.txt", sep = ";", header = TRUE, na.strings = "?")
+
+#formating date
 date.format <- as.Date(strptime(data$Date, "%d/%m/%Y"))
 hpower <- cbind(data, date.format)
+
+#subseting for the first two days of February
 day1 <- hpower[hpower$date.format == "2007-02-01", ]
 day2 <- hpower[hpower$date.format == "2007-02-02", ]
 hpower.subset <- rbind(day1, day2)
+
+#Creating the plot
+png(file = "plot1.png", width = 480, height = 480)
 hist(hpower.subset$Global_active_power, col = "red", xlab = "Global Active Power (kilowatts)", main = ("Global Active Power"))
 title(main="Global Active Power")
+dev.off()
