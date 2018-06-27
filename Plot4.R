4f89680d1f4f660bb4818ea5f702fff014ad79be
MTOliveira <38473374+MTOliveira@users.noreply.github.com>
2018-06-27 10:46:36 +0100

    Update Plot4.R

--- Plot4.R
+++ Plot4.R
@@ -1,13 +1,24 @@
 ## Plot 4 
+
+#read the dataset
 data <- (read.table("R datasets/household_power_consumption.txt", sep = ";", header = TRUE, na.strings = "?"))
+
+#formating date
 data$Date <- as.Date(data$Date, "%d/%m/%Y")
+
+#subseting for the first two days of February
 day1 <- data[data$Date == "2007-02-01", ]
 day2 <- data[data$Date == "2007-02-02", ]
 hpower <- rbind(day1, day2)
+
+#formating time
 hpower$Time <- strptime(hpower$Time, format="%H:%M:%S")
 hpower[1:1440,"Time"] <- format(hpower[1:1440,"Time"],"2007-02-01 %H:%M:%S")
 hpower[1441:2880,"Time"] <- format(hpower[1441:2880,"Time"],"2007-02-02 %H:%M:%S")
-#setting the grid structure
+
+#Creating the plot
+png(file = "plot4.png", width = 480, height = 480)
+#setting the 2x2 grid
 par(mfrow = c(2,2))
 #plot1
 plot(hpower$Time, as.numeric(as.character(hpower$Global_active_power)), type="l",xlab="",ylab="Global Active Power (kilowatts)")
@@ -20,3 +31,5 @@ lines(hpower$Time, hpower$Sub_metering_3, type = "l", col="blue")
 legend("topright", lty =1, col = c("black", "red", "blue"), legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), bty = "n")
 #plot4
 plot(hpower$Time, as.numeric(as.character(hpower$Global_reactive_power)), type="l",xlab="datetime",ylab="Global_reactive_power")
+
+dev.off()
